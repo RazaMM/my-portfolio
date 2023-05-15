df389d477aa4b83b644c576d1149ff574ad37803
Raza Mahmood <razammahmood@gmail.com>
2023-05-15 03:57:28 -0400

    formatting

--- src/components/MyBio.svelte
+++ src/components/MyBio.svelte
@@ -2,9 +2,12 @@
   import me from '../img/me.png';
 </script>
 
-<div class="flex flex-col gap-4 items-center justify-center p-2 bg-white text-center w-[24rem] h-[32rem] max-w-full max-h-full">
-  <img class="w-56 rounded-[50%]" src={me} alt="A picture of Raza Mahmood wearing sunglasses" />
+<div
+  class="flex flex-col gap-4 items-center justify-center p-2 bg-white text-center w-[24rem] h-[32rem] max-w-full max-h-full"
+>
+  <img class="w-56 rounded-[50%]" src={me} alt="A picture of Raza Mahmood wearing sunglasses"/>
   <h1 class="text-xl">Raza Mahmood</h1>
-  <p>I am a 5th year computer science co-op student at the University of Guelph who specializes in front end web development.</p>
+  <p>I am a 5th year computer science co-op student at the University of Guelph who specializes in front end web
+    development.</p>
   <a class="text-w95-blue" href="mailto:razammahmood@gmail.com">razammahmood@gmail.com</a>
 </div>

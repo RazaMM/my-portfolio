9b62e7382916f3c63f7e4457d4deb2cf19ad39f6
Raza Mahmood <razammahmood@gmail.com>
2023-05-20 18:03:14 -0400

    consolidating components in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import Window from "$components/Window.svelte";
     import { activeProgram, openPrograms, programs } from "$lib/programs.ts";
-    import DesktopIcon from "$components/DesktopIcon.svelte";
 </script>
 
 <svelte:head>
@@ -9,9 +8,11 @@
 </svelte:head>
 
 <div class="flex flex-col flex-wrap items-center gap-4 w-fit max-h-[calc(100vh-40px)] p-2">
-
     {#each programs as program}
-        <DesktopIcon program={program}/>
+        <button class="flex flex-col items-center gap-0.5 w-fit h-fit" on:dblclick={() => openPrograms.open(program)}>
+            <img class="h-10 rendering-pixelated" src={program.icon} alt="{program.name} icon"/>
+            <span class="text-white text-center">{program.name}</span>
+        </button>
     {/each}
 </div>
 
@@ -20,13 +21,9 @@
         name={program.name}
         icon={program.icon}
         active={$activeProgram === program}
-        resizable={program.resizeableWindow}
-        on:close={() => {
-      openPrograms.close(program);
-    }}
-        on:mousedown={() => {
-      activeProgram.set(program);
-    }}
+        resizable={program.resizeable}
+        on:close={() => openPrograms.close(program)}
+        on:mousedown={() => activeProgram.set(program)}
     >
         <svelte:component this={program.component}/>
     </Window>

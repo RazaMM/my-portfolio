db73afdbd6a584f9f2a306e833e2613601af9938
Raza Mahmood <razammahmood@gmail.com>
2023-05-20 18:23:26 -0400

    making start menu close when focus is no longer within it

--- src/components/Taskbar.svelte
+++ src/components/Taskbar.svelte
@@ -2,7 +2,9 @@
     import {activeProgram, openPrograms, programs} from "$lib/programs.ts";
     import {onMount} from "svelte";
     import logo from "$icons/windows.png";
+    import {focusWithin} from "$lib/focus-within.ts";
 
+    /* Clock logic */
     let hours = "0";
     let minutes = "0";
     let amOrPm = "AM";
@@ -22,44 +24,49 @@
         return () => clearInterval(interval);
     });
 
+    /* Start Menu logic */
     let startMenuOpen = false;
 </script>
 
 <footer class="relative flex items-center w-full p-2 gap-6 bg-w95-grey shadow-w95 select-none z-50">
-    <!-- Start Menu Button -->
-    <button
-        class="flex items-center gap-1 px-2 shadow-w95 whitespace-nowrap active:shadow-w95-inverted"
-        class:shadow-w95-inverted={startMenuOpen}
-        on:click={() => startMenuOpen = !startMenuOpen}
-    >
-        <img src={logo} alt="w icon" class="h-6 rendering-pixelated"/>
-        <span class="block h-full overflow-hidden text-ellipsis whitespace-nowrap">Start</span>
-    </button>
 
-    <!-- Start Menu Content-->
-    <div class="flex w-80 max-w-full gap-2 p-1.5 absolute bottom-full left-0 bg-w95-grey shadow-w95" class:invisible={!startMenuOpen}>
-        <!-- Sideways Text -->
-        <span class="flex items-center w-8 py-2 text-xl text-white rotate-180 bg-w95-dark-grey vertical-writing-lr orientation-sideways tracking-widest">Portfolio 95</span>
+    <!-- Start Menu -->
+    <div use:focusWithin on:focusoutside={() => startMenuOpen = false} tabindex="-1">
+        <!-- Start Menu Button -->
+        <button
+            class="flex items-center gap-1 px-2 shadow-w95 whitespace-nowrap active:shadow-w95-inverted"
+            class:shadow-w95-inverted={startMenuOpen}
+            on:click={() => startMenuOpen = !startMenuOpen}
+        >
+            <img src={logo} alt="w icon" class="h-6 rendering-pixelated"/>
+            <span class="block h-full overflow-hidden text-ellipsis whitespace-nowrap">Start</span>
+        </button>
 
-        <!-- Start Menu Items -->
-        <div class="flex-1 flex flex-col justify-end">
-            {#each programs as program}
-                <button
-                    class="flex items-center gap-1 p-1 w-full h-10 hover:bg-w95-blue hover:text-white focus:outline-none focus:bg-w95-blue focus:text-white"
-                    on:click={() => openPrograms.open(program)}
-                >
-                    {#if program.icon}
-                        <img src={program.icon} alt="{program.name} icon" class="h-full rendering-pixelated"/>
-                    {/if}
-                    <span>{program.name}</span>
+        <!-- Start Menu Content-->
+        <div class="flex w-80 max-w-full gap-2 p-1.5 absolute bottom-full left-0 bg-w95-grey shadow-w95" class:invisible={!startMenuOpen}>
+            <!-- Sideways Text -->
+            <span class="flex items-center w-8 py-2 text-xl text-white rotate-180 bg-w95-dark-grey vertical-writing-lr orientation-sideways tracking-widest">Portfolio 95</span>
+
+            <!-- Start Menu Items -->
+            <div class="flex-1 flex flex-col justify-end">
+                {#each programs as program}
+                    <button
+                        class="flex items-center gap-1 p-1 w-full h-10 hover:bg-w95-blue hover:text-white focus:outline-none focus:bg-w95-blue focus:text-white"
+                        on:click={() => openPrograms.open(program)}
+                    >
+                        {#if program.icon}
+                            <img src={program.icon} alt="{program.name} icon" class="h-full rendering-pixelated"/>
+                        {/if}
+                        <span>{program.name}</span>
+                    </button>
+                {/each}
+
+                <hr class="h-2 shadow-windows w-full"/>
+
+                <button class="flex items-center gap-1 p-1 w-full h-10 hover:bg-w95-blue hover:text-white focus:outline-none focus:bg-w95-blue focus:text-white">
+                    <span>Shut Down...</span>
                 </button>
-            {/each}
-
-            <hr class="h-2 shadow-windows w-full"/>
-
-            <button class="flex items-center gap-1 p-1 w-full h-10 hover:bg-w95-blue hover:text-white focus:outline-none focus:bg-w95-blue focus:text-white">
-                <span>Shut Down...</span>
-            </button>
+            </div>
         </div>
     </div>
 

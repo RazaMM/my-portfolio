4001463aff33456a285abd2b8ba9870deb4b1e7d
Raza Mahmood <razammahmood@gmail.com>
2023-05-20 21:23:42 -0400

    made seven segment display component for Minesweeper

--- src/components/Minesweeper.svelte
+++ src/components/Minesweeper.svelte
@@ -1,3 +1,7 @@
-<div>
+<script>
+    import SevenSegmentDisplay from "$components/SevenSegmentDisplay.svelte";
+</script>
 
+<div class="w-fit h-fit text-4xl">
+    <SevenSegmentDisplay value="000" />
 </div>

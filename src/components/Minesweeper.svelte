74e1d5c914bba3401a422f0394e4ab73d482692a
Raza Mahmood <razammahmood@gmail.com>
2023-05-21 00:06:02 -0400

    working on minesweeper

--- src/components/Minesweeper.svelte
+++ src/components/Minesweeper.svelte
@@ -1,7 +1,19 @@
 <script>
     import SevenSegmentDisplay from "$components/SevenSegmentDisplay.svelte";
+
+    export let width = 9;
+    export let height = 9;
 </script>
 
-<div class="w-fit h-fit text-4xl">
-    <SevenSegmentDisplay value="000" />
+<div class="flex flex-col gap-2 p-2 shadow-w95-alt">
+    <div class="flex text-3xl justify-between items-center shadow-w95-alt-inverted gap-2 p-2">
+        <SevenSegmentDisplay value="000" />
+        <button class="w-6 h-6 shadow-w95-alt active:shadow-w95-alt-inverted"></button>
+        <SevenSegmentDisplay value="000" />
+    </div>
+    <div class="grid gap-1" style={`grid-template-columns: repeat(${width}, 1rem);`}>
+        {#each Array(width * height) as _, i}
+           <button class="flex items-center justify-center w-4 h-4 shadow-w95-alt-thin active:shadow-w95-alt-thin-inverted"></button>
+        {/each}
+    </div>
 </div>
